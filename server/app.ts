6f88914f7343edb18b137f9a6372765f6335f188
Mutethia-glitch <promisemutethia@gmail.com>
2026-09-05 16:13:29 +0000

    Isolate Vercel API startup failure

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { registerOAuthRoutes } from "./_core/oauth";
-import { registerLocalAuthRoutes } from "./_core/localAuth";
 import { registerStorageProxy } from "./_core/storageProxy";
 import { appRouter } from "./routers";
 import { createContext } from "./_core/context";
@@ -12,7 +11,6 @@ export function createApp() {
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
   registerStorageProxy(app);
   registerOAuthRoutes(app);
-  registerLocalAuthRoutes(app);
   app.use("/api/trpc", createExpressMiddleware({ router: appRouter, createContext }));
   return app;
 }
