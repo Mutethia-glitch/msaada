1ccb2462d9785544fb409eed8ad49bb4a0475077
Mutethia-glitch <promisemutethia@gmail.com>
2026-09-05 16:20:45 +0000

    Expose account auth directly from Vercel API

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import { registerLocalAuthRoutes } from "../server/_core/localAuth";
 
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+registerLocalAuthRoutes(app);
 
 export default function api(req: express.Request, res: express.Response) {
   return app(req, res);
