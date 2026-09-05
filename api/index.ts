8a781b5cb8bb62a2550cfac376effee205f964c8
Mutethia-glitch <promisemutethia@gmail.com>
2026-09-05 16:19:57 +0000

    Inline Vercel Express bootstrap

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,9 @@
-import type { Request, Response } from "express";
-import { createApp } from "../server/app";
+import express from "express";
 
-const app = createApp();
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-export default function api(req: Request, res: Response) {
+export default function api(req: express.Request, res: express.Response) {
   return app(req, res);
 }
