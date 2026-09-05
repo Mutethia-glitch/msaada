38a3c260cc2245c57c2eb07e400ebfac3caa39db
Mutethia-glitch <promisemutethia@gmail.com>
2026-09-05 16:25:09 +0000

    Restore full Vercel application routes

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,7 @@
 import express from "express";
-import { registerLocalAuthRoutes } from "../server/_core/localAuth";
+import { createApp } from "../server/app";
 
-const app = express();
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-registerLocalAuthRoutes(app);
+const app = createApp();
 
 export default function api(req: express.Request, res: express.Response) {
   return app(req, res);
